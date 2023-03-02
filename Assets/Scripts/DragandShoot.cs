6a438de9f2601a20797bec88df1f3b373f43d6b9
ArdaDenizKara <ardadenizkara35@gmail.com>
2023-03-02 11:15:35 +0300

    refactor(code) : edited DragAndShoot script

--- Assets/Scripts/DragandShoot.cs
+++ Assets/Scripts/DragandShoot.cs
@@ -5,7 +5,8 @@ using Generic.Enums;
 
 public class DragandShoot : MonoBehaviour
 {
-    Vector3 distance = new Vector3(4.2f,0,0);
+    #region Fields
+    Vector3 distance = new Vector3(4.2f, 0, 0);
     public float power;
     private float enemyPower = 300f;
     public Rigidbody2D rb;
@@ -24,60 +25,43 @@ public class DragandShoot : MonoBehaviour
     public AudioSource audioSource;
     [SerializeField]
     GamePlay gameplay;
-    
     bool isEnemyTurnCalled = false;
     bool isPlayerTurnCalled = true;
-    
-    
     BallManager ballManager;
+    #endregion
+    #region Functions
     public void Start()
     {
-        
         gameplay = GetComponent<GamePlay>();
         cam = Camera.main;
         lr = GetComponent<LineRendering>();
-        
         StartCoroutine(StartState());
         whiteBallPos = GameObject.FindGameObjectWithTag("WhiteBall").transform.position;
         whiteBallRotation = GameObject.FindGameObjectWithTag("WhiteBall").transform.rotation;  /*ballManager.whiteBallList[0].transform.rotation;*/
-
     }
-    
     public void Update()
     {
-        
-        //gameplay.gameplayState = Generic.Enums.GamePlayState.PlayerTurn;
         bool isGameplayStateStart = GameManager.Instance.gameplayState == Generic.Enums.GamePlayState.Start;
         bool isPlayerTurn = GameManager.Instance.gameplayState == Generic.Enums.GamePlayState.PlayerTurn;
         bool isEnemyTurn = GameManager.Instance.gameplayState == Generic.Enums.GamePlayState.OpponentTurn;
         bool isBallNotMoving = rb.velocity == Vector2.zero && rb.angularVelocity == 0;
-        //StopCoroutine(StartState());
-
-        if (isPlayerTurn && isPlayerTurnCalled&&isBallNotMoving)
-
+        if (isPlayerTurn && isPlayerTurnCalled && isBallNotMoving)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
                 startPoint.z = 15;
-                
-                //TODO mouse hareket ettiginde ýstakaya rotation gönder
-
-
             }
             if (Input.GetMouseButton(0))
             {
                 Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
                 currentPoint.z = 15;
-                whiteBallPos = GameObject.FindGameObjectWithTag("WhiteBall").transform.position ;
+                whiteBallPos = GameObject.FindGameObjectWithTag("WhiteBall").transform.position;
                 whiteBallRotation = GameObject.FindGameObjectWithTag("WhiteBall").transform.rotation;
-                
-                //GameManager.Instance.GetCue(whiteBallPos-distance,whiteBallPos);
                 lr.RenderLine(startPoint, currentPoint);
             }
             if (Input.GetMouseButtonUp(0))
             {
-                //StopCoroutine(EnemyTurn());
                 endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
                 endPoint.z = 15;
                 force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
@@ -86,25 +70,18 @@ public class DragandShoot : MonoBehaviour
                 StartCoroutine(EnemyTurn());
                 isEnemyTurnCalled = false;
                 isPlayerTurnCalled = false;
-                //GameManager.Instance.RemoveCue();
                 SoundManager.Instance.PlaySound();
-                
             }
-           
         }
-       else if (isEnemyTurn && isEnemyTurnCalled==false && isBallNotMoving)
+        else if (isEnemyTurn && isEnemyTurnCalled == false && isBallNotMoving)
         {
-            //TODO ENEMYE ISTAKA EKLE
-                rb.AddForce(force * enemyPower, ForceMode2D.Force);//transform.up * enemyPower);
+            rb.AddForce(force * enemyPower, ForceMode2D.Force);
             SoundManager.Instance.PlaySound();
-                StartCoroutine(PlayerTurn());
+            StartCoroutine(PlayerTurn());
             isPlayerTurnCalled = true;
-            isEnemyTurnCalled = true;          
-            
+            isEnemyTurnCalled = true;
         }
-        
     }
-    //TODO (ADK): enemy turu hýzlý geçtiði için player top durmadan vurabilir.
     IEnumerator PlayerTurn()
     {
         yield return new WaitForSeconds(5f);
@@ -114,13 +91,11 @@ public class DragandShoot : MonoBehaviour
     {
         yield return new WaitForSeconds(5f);
         GameManager.Instance.gameplayState = Generic.Enums.GamePlayState.OpponentTurn;
-        
-
     }
     IEnumerator StartState()
     {
         yield return new WaitForSeconds(1f);
         GameManager.Instance.gameplayState = Generic.Enums.GamePlayState.PlayerTurn;
     }
-    
+    #endregion
 }
