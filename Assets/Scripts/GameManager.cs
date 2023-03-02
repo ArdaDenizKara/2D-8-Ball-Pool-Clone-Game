6574ffda37d2d8897db0c241d2daacd6156cfd27
ArdaDenizKara <ardadenizkara35@gmail.com>
2023-03-02 11:27:56 +0300

    refactor(code) : edited GameManager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -47,33 +47,25 @@ public class GameManager : MonoBehaviour
         SpawnRack();
         StartCoroutine(DestroyRack());
         StopCoroutine(DestroyRack());
-        
     }
     public void SpawnRack()
     {
         Instantiate(rack, new Vector3(3.0315f, 0.13f, 0), rack.transform.rotation);
-
-
     }
     IEnumerator DestroyRack()
     {
-
         yield return new WaitForSeconds(1f);
         Destroy(GameObject.FindGameObjectWithTag("Rack"));
-
     }
     public void PlayerWon()
     {
         playerWinScreen.SetActive(true);
         playerWinText.text = "You Win ! ";
-       
-       
     }
     public void OpponentWon()
     {
         opponentWinScreen.SetActive(true);
         opponentWinText.text = "You Lose!";
-        
     }
     public void StartGameButton()
     {
@@ -83,32 +75,5 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
-    //public void IniteCue()
-    //{
-    //    foreach (GameObject cue in cuePrefabList)
-    //    {
-    //        var cueObject = GameObject.Instantiate(cue);
-    //        cueList.Add(cueObject);
-    //        cueObject.SetActive(false);
-    //    }
-        
-    //}
-    //public GameObject GetCue(Vector3 position, Vector3 rotation)
-    //{
-    //    var cue =  cueList.FirstOrDefault();
-    //    cue.transform.position = position;
-    //    cue.transform.rotation = Quaternion.Euler(rotation);
-
-    //    cue.SetActive(true);
-    //    return cue;
-    //}
-    //public void RemoveCue()
-    //{
-    //    var cue = cueList.FirstOrDefault();
-    //    cue.SetActive(false);
-
-
-
-    //}
     #endregion
 }
